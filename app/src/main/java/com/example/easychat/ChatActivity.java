c9ea1f4ad9c6ab16aaae8158eb579c23e4fbb211
cwh <bygzyx@outlook.com>
2024-05-29 19:21:17 +0800

    v0.2_backBtn_Bug

--- app/src/main/java/com/example/easychat/ChatActivity.java
+++ app/src/main/java/com/example/easychat/ChatActivity.java
@@ -2,11 +2,13 @@ package com.example.easychat;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -43,17 +45,35 @@ public class ChatActivity extends AppCompatActivity {
         otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
 
         messageInput = findViewById(R.id.chat_message_input);
-//        sendMessageBtn = findViewById(R.id.message_send_btn);
+        sendMessageBtn = findViewById(R.id.message_send_btn);
         backBtn = findViewById(R.id.back_btn);
         otherUsername = findViewById(R.id.other_username);
         recyclerView = findViewById(R.id.chat_recycler_view);
 
+        // Create a callback for onBackPressed
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                if (this.isEnabled()) {
+                    this.setEnabled(false); // Disable the callback
+                    finish();
+                }
+//                getOnBackPressedDispatcher().onBackPressed();
+            }
+        };
+
+        // Add the callback to the onBackPressedDispatcher
+        getOnBackPressedDispatcher().addCallback(this, callback);
+
         backBtn.setOnClickListener(v -> {
-            onBackPressed();
+            // Trigger the callback when back button is pressed
+            callback.handleOnBackPressed();
         });
         otherUsername.setText(otherUser.getUsername());
 
 
 
     }
+
 }
